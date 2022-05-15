c9b3de39c7d8cac9401cfc9cb40e402a78d34b16
kadekframa <dframa19@gmail.com>
2022-05-15 17:03:00 +0800

    Movie Catalogue. Create and Implements Service Worker in this projects. Keep Spirit!!!

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const path = require('path');
-
+ 
 module.exports = {
   entry: path.resolve(__dirname, 'src/scripts/index.js'),
   output: {
@@ -35,6 +36,9 @@ module.exports = {
           to: path.resolve(__dirname, 'dist/'),
         },
       ],
+    }),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.resolve(__dirname, 'src/scripts/sw.js'),
     }),
   ],
 };
