1e3f05d05527a43bd8ba121c00926736a88d3ec3
kadekframa <dframa19@gmail.com>
2022-05-16 21:52:00 +0800

    Movie Catalogue. Implements Websocket to this project. Keep Spirit!!!

---
+++ src/scripts/utils/websocket-initiator.js
@@ -0,0 +1,12 @@
+const WebSocketInitiator = {
+    init(url) {
+        const webSocket = new WebSocket(url);
+        webSocket.onmessage = this._onMessageHandler;
+    },
+
+    _onMessageHandler(message) {
+        console.info(message.data);
+    },
+};
+
+export default WebSocketInitiator;
